1e4b25e9b41bb7db750093708a45423e68a4ec7d
Stefano Florio <31825478+StephanXV@users.noreply.github.com>
2019-03-07 12:56:46 +0100

    nada

--- app/src/main/java/it/univaq/mobileprogramming/uniweather/model/WeatherResult.java
+++ app/src/main/java/it/univaq/mobileprogramming/uniweather/model/WeatherResult.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 public class WeatherResult {
 
-    public Coord coord;
-    public List<Weather> weather;
-    public String base;
-    public Main main;
-    public int visibility;
-    public Wind wind;
-    public Rain rain;
-    public Clouds clouds;
-    public int dt;
-    public Sys sys;
-    public int id;
-    public String name;
-    public int cod;
+    private Coord coord;
+    private List<Weather> weather;
+    private String base;
+    private Main main;
+    private int visibility;
+    private Wind wind;
+    private Rain rain;
+    private Clouds clouds;
+    private int dt;
+    private Sys sys;
+    private int id;
+    private String name;
+    private int cod;
+
+    public WeatherResult(){}
 
     public WeatherResult(Coord coord, List<Weather> weather, String base, Main main, int visibility, Wind wind, Rain rain, Clouds clouds, int dt, Sys sys, int id, String name, int cod) {
         this.coord = coord;

c007a0fd09cc81c0c1bea48d269e71893453e5be
im-slow <andrea.amicosante@gmail.com>
2019-03-07 16:22:00 +0100

    titolo al centro

--- app/src/main/java/it/univaq/mobileprogramming/uniweather/activity/MainActivity.java
+++ app/src/main/java/it/univaq/mobileprogramming/uniweather/activity/MainActivity.java
@@ -1,17 +1,22 @@
 package it.univaq.mobileprogramming.uniweather.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import it.univaq.mobileprogramming.uniweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
     }
+
 }
